cc1d2bb51a0491a7d61bc2f77250efdfda2b8947
BURNSIDE HIGH SCHOOL <code@burnside.school.nz>
2024-04-09 08:59:56 +1200

    first commit

---
+++ test.py
@@ -0,0 +1,15 @@
+import sqlite3
+
+DATABASE = 'cars.db'
+
+with sqlite3.connect(DATABASE) as db:
+    db = sqlite3.connect('cars.db')
+    cursor = db.cursor()
+    # SQLite Query
+    sql = "SELECT * FROM car ORDER BY top_speed DESC;"
+
+    cursor.execute(sql)
+    results = cursor.fetchall()
+    # print results
+    for car in results:
+        print(car)
