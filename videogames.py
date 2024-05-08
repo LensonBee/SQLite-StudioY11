f2c988624a73a489e83d5d213ecd03efd8bfd511
BURNSIDE HIGH SCHOOL <code@burnside.school.nz>
2024-05-08 14:15:25 +1200

    Made videogames.py print nicer

--- videogames.py
+++ videogames.py
@@ -6,16 +6,16 @@ DATABASE = 'videogames.db'
 
 
 def print_all_games():
-    with sqlite3.connect(DATABASE) as db:
-        cursor = db.cursor()
-        # SQLite Query
-        sql = "SELECT * FROM game;"
-        cursor.execute(sql)
-        results = cursor.fetchall()
-        # print results
-
-    for car in results:
-        print(f"{game}")
+    db = sqlite3.connect(DATABASE)
+    cursor = db.cursor()
+    # SQLite Query
+    sql = "SELECT * FROM game;"
+    cursor.execute(sql)
+    results = cursor.fetchall()
+    # print results
+    print("Name                          Genre")
+    for game in results:
+        print(f"{game[1]:<30}{game[2]:<8}")
 
 
 if __name__ == "__main__":
