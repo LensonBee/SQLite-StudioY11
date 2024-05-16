01095b8a246c9310340df44a334333a8a5653942
BURNSIDE HIGH SCHOOL <code@burnside.school.nz>
2024-05-16 14:15:19 +1200

    added buggy custom query function

--- videogames.py
+++ videogames.py
@@ -8,16 +8,35 @@ DATABASE = 'videogames.db'
 
 # functions
 def print_all_games():
-    # print all games nicely
     db = sqlite3.connect(DATABASE)
     cursor = db.cursor()
     sql = "SELECT * FROM game;"
     cursor.execute(sql)
     results = cursor.fetchall()
     # loop through all results
-    print("Name                          Genre               Rating         Studio_id")
+    print("Name                               Genre", end='')
+    print("               Rating         Studio_id")
     for game in results:
-        print(f"{game[1]:<30}{game[2]:<20}{game[3]:<20}{game[4]:<15}")
+        print(f"{game[1]:<35}{game[2]:<20}{game[3]:<15}{game[4]}")
+    # loop finished here
+    db.close()
+
+
+def custom_query():
+    print("Enter custom query below", end="")
+    try:
+        userquery = str(input(": \n"))
+    except ValueError:
+        print("Invalid query")
+    db = sqlite3.connect(DATABASE)
+    cursor = db.cursor()
+    cursor.execute(userquery)
+    results = cursor.fetchall()
+    # loop through all results
+    print("Name                               Genre", end='')
+    print("               Rating         Studio_id")
+    for game in results:
+        print(f"{game[1]:<35}{game[2]:<20}{game[3]:<15}{game[4]}")
     # loop finished here
     db.close()
 
@@ -28,12 +47,13 @@ while True:
         """
         What would you like to do.
         1. Print all games
+        2. Custom query
         7. Exit
         """)
     if user_input == "1":
         print_all_games()
     elif user_input == "2":
-        pass
+        custom_query()
     elif user_input == "7":
         break
     else:
