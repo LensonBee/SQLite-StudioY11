bcaae3e4a58b2d219399bef2dfd5ab09f7da9f22
BURNSIDE HIGH SCHOOL <code@burnside.school.nz>
2024-05-20 12:09:36 +1200

    funni

--- videogames.py
+++ videogames.py
@@ -14,10 +14,44 @@ def print_all_games():
     cursor.execute(sql)
     results = cursor.fetchall()
     # loop through all results
-    print("Name                               Genre", end='')
-    print("               Rating         Studio_id")
+    print("Name                                    Genre", end='')
+    print("               Rating              Studio_id")
     for game in results:
-        print(f"{game[1]:<35}{game[2]:<20}{game[3]:<15}{game[4]}")
+        print(f"{game[1]:<40}{game[2]:<20}{game[3]:<20}{game[4]}")
+    # loop finished here
+    db.close()
+
+
+def search_game():
+    search = str(input("Search for a game below: \n"))
+    search = '%' + search + '%'
+    db = sqlite3.connect(DATABASE)
+    cursor = db.cursor()
+    sql = "SELECT * FROM game WHERE name LIKE ?;"
+    cursor.execute(sql, (search,))
+    results = cursor.fetchall()
+    # loop through all results
+    print("Name                                    Genre", end='')
+    print("               Rating              Studio_id")
+    for game in results:
+        print(f"{game[1]:<40}{game[2]:<20}{game[3]:<20}{game[4]}")
+    # loop finished here
+    db.close()
+
+
+def search_studio():
+    search = str(input("Search for a studio below: \n"))
+    search = '%' + search + '%'
+    db = sqlite3.connect(DATABASE)
+    cursor = db.cursor()
+    sql = "SELECT * FROM studio WHERE studio_name LIKE ?;"
+    cursor.execute(sql, (search,))
+    results = cursor.fetchall()
+    # loop through all results
+    print("Name                                    Genre", end='')
+    print("               Rating              Studio_id")
+    for game in results:
+        print(f"{game[1]:<40}{game[2]:<20}{game[3]:<20}{game[4]}")
     # loop finished here
     db.close()
 
@@ -26,17 +60,40 @@ def custom_query():
     print("Enter custom query below", end="")
     try:
         userquery = str(input(": \n"))
-    except ValueError:
+        db = sqlite3.connect(DATABASE)
+        cursor = db.cursor()
+        cursor.execute(userquery)
+        results = cursor.fetchall()
+        # loop through all results
+        print("Name                                    Genre", end='')
+        print("               Rating              Studio_id")
+        for game in results:
+            print(f"{game[1]:<40}{game[2]:<20}{game[3]:<20}{game[4]}")
+    except:
         print("Invalid query")
-    db = sqlite3.connect(DATABASE)
-    cursor = db.cursor()
-    cursor.execute(userquery)
-    results = cursor.fetchall()
-    # loop through all results
-    print("Name                               Genre", end='')
-    print("               Rating         Studio_id")
-    for game in results:
-        print(f"{game[1]:<35}{game[2]:<20}{game[3]:<15}{game[4]}")
+        return
+        custom_query()
+    # loop finished here
+    db.close()
+
+
+def add_element():
+    print("Enter new element below (INSERT INTO game ())", end="")
+    try:
+        element = str(input(": \n"))
+        db = sqlite3.connect(DATABASE)
+        cursor = db.cursor()
+        cursor.execute(userquery)
+        results = cursor.fetchall()
+        # loop through all results
+        print("Name                                    Genre", end='')
+        print("               Rating              Studio_id")
+        for game in results:
+            print(f"{game[1]:<40}{game[2]:<20}{game[3]:<20}{game[4]}")
+    except:
+        print("Invalid query")
+        return
+        custom_query()
     # loop finished here
     db.close()
 
@@ -47,12 +104,19 @@ while True:
         """
         What would you like to do.
         1. Print all games
-        2. Custom query
+        2. Search by name
+        3. Search by studio
+        4. Custom query
+        5. Add element (not done)
         7. Exit
         """)
     if user_input == "1":
         print_all_games()
     elif user_input == "2":
+        search_game()
+    elif user_input == "3":
+        search_studio()
+    elif user_input == "4":
         custom_query()
     elif user_input == "7":
         break
