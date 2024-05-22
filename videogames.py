684fd7202a640ef91e7bf0c358eb2f5c13cfb395
BURNSIDE HIGH SCHOOL <code@burnside.school.nz>
2024-05-22 14:15:30 +1200

    new stuff I guess

--- videogames.py
+++ videogames.py
@@ -22,6 +22,20 @@ def print_all_games():
     db.close()
 
 
+def print_all_studios():
+    db = sqlite3.connect(DATABASE)
+    cursor = db.cursor()
+    sql = "SELECT * FROM studio;"
+    cursor.execute(sql)
+    results = cursor.fetchall()
+    # loop through all results
+    print("\nID                  Name")
+    for studio in results:
+        print(f"{studio[0]:<20}{studio[1]}")
+    # loop finished here
+    db.close()
+
+
 def search_game():
     try:
         search = str(input("Search for a game below: \n"))
@@ -59,6 +73,63 @@ def search_studio():
     db.close()
 
 
+def add_element():
+    # ask user for which table
+    user_input = input("""
+        Choose a table to add to.
+        1. Games
+        2. Studios
+        """)
+    if user_input == "1":
+        print("not added yet")
+    elif user_input == "2":
+        add_studio()
+    else:
+        print("\nThat was not an option")
+
+
+def add_studio():
+    # ask user for new studio name
+    print("\nEnter new studio below")
+    new_name = input()
+    db = sqlite3.connect(DATABASE)
+    cursor = db.cursor()
+    # insert new studio
+    sql = "INSERT INTO studio (studio_name) VALUES (?);"
+    cursor.execute(sql, (new_name,))
+    db.commit()  # commit changes to table
+    print("Studio added")
+    db.close()
+
+
+def delete_element():
+    # ask user for which table
+    user_input = input("""
+        Choose a table to delete something from.
+        1. Games
+        2. Studios
+        """)
+    if user_input == "1":
+        print("not added yet")
+    elif user_input == "2":
+        delete_studio()
+    else:
+        print("\nThat was not an option")
+
+
+def delete_studio():
+    # ask user for studio name to remove
+    print("\nEnter studio to delete below")
+    delete_name = input()
+    db = sqlite3.connect(DATABASE)
+    cursor = db.cursor()
+    # delete studio
+    sql = ";"
+    cursor.execute(sql, (delete_name,))
+    db.commit()  # commit changes to table
+    print("Studio added")
+    db.close()
+
 
 # main code
 while True:
@@ -66,20 +137,26 @@ while True:
         """
         What would you like to do.
         1. Print all games
-        2. Search by name
-        3. Search by studio
-        4. Add element (not done)
-        7. Exit
+        2. Print all studios
+        3. Search by game
+        4. Search by studio
+        5. Add element (work in progress)
+        6. Delete element (work in progress)
+        9. Exit
         """)
     if user_input == "1":
         print_all_games()
     elif user_input == "2":
-        search_game()
+        print_all_studios()
     elif user_input == "3":
+        search_game()
+    elif user_input == "4":
         search_studio()
-    elif user_input == "7":
+    elif user_input == "5":
+        add_element()
+    elif user_input == "9":
         break
     else:
         print("\nThat was not an option")
 
-# I'm racist™
+# Quantum Tunneling™
